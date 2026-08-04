bf331d5a57671529b0e11620e0503ea9efccb974
Parthiban K <parthibansk007@gmail.com>
2026-08-04 12:30:47 +0530

    Updates

---
+++ assets/Card-CCul-CU3.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-w-LgmH03.js";var t=e(),n=({children:e,className:n=``,...r})=>(0,t.jsx)(`div`,{className:`rounded-[28px] border border-white/10 bg-slate-900/60 p-6 backdrop-blur-xl ${n}`,...r,children:e});export{n as t};
